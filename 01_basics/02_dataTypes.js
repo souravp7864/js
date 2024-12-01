8907abbd07285cabc75d51ce3f308241b38ed93d
souravp7864 <souravp7864@gmail.com>
2024-12-01 04:15:04 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)     //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)   //code redability should be high
+
+console.log("sourav");
+let name="sourav"
+let age= 18
+let isLoggedIn=false
+//number=> 2 to the power 53
+// biginit 
+// string=>""
+//boolean=> true/false
+// null=> standalone value
+// undefined =>
+// symbol=> unique
+
+//object
+console.log(typeof unll);  //object
+console.log(typeof undefined);   //undefined
+
