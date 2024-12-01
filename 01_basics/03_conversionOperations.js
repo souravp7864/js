50f74831fb396eeddd82a86477cf73571b9d6acf
souravp7864 <souravp7864@gmail.com>
2024-12-01 07:50:16 +0000

    conversion data type

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,25 @@
-let score= "33"
+let score= "sourav"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber=Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber); //NaN = Not a Number
+
+//"33"=>33
+// "33abc"=> NaN(Not a Number)
+//true=> 1; false => 0
+
+let isLoggedIn="sourav"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true;  0 => false
+// "" = > false
+// "sourav" => true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
